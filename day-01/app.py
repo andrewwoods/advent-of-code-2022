4ed6fa2b4abdacc5c720fb8d8328e91099df5f14
Andrew Woods <services@awoods.net>
2022-12-02 09:32:20 -0500

    Refactor Day 01 to use arrays
    Using the individual counters was error prone. Using arrays made things
    simpler to handle.

--- day-01/app.py
+++ day-01/app.py
@@ -4,45 +4,26 @@ from pprint import pprint
 
 
 data_file = "day-01/data.txt"
-data_file = "day-01/elf-calories-test-data.txt"
+# Test data - use for debugging
+# data_file = "day-01/elf-calories-test-data.txt"
 
 elf_index = 0
 elf_total = 0
-elf_max = 0
-baller_elf = 0
-grand_total = 0
 elves = []
 
 f = open(data_file,"r")
-lines = f.readlines()
 
-for line in lines:
+for line in f.readlines():
     print(line, end="")
 
     if line == "\n":
-
-        if elf_total > elf_max:
-            elf_max = elf_total
-            baller_elf = elf_index
-
         elves.append(elf_total) 
-        grand_total += elf_total
-
-        # Reset values
         elf_total = 0
-        elf_index += 1
-        continue
-
-    elf_total += int(line)
-
-if elf_total > elf_max:
-    elf_max = elf_total
-    baller_elf = elf_index
+    else:
+        elf_total += int(line)
 
 elves.append(elf_total)
-grand_total += elf_total
 
-print("Elf Total=", elf_total, "\n", end="")
-print("Baller Elf=", baller_elf, "\n", end="")
-print("Grand Total=", grand_total, "\n", end="")
+print("Part 1 result:", max(elves) )
 
+print("Part 2 result:", sum(sorted(elves,reverse=True)[0:3]) )
