77eb9e1d9a6ca40c13d52db040bea93db04309bb
Andrew Woods <services@awoods.net>
2022-12-03 19:26:16 -0500

    Solved Day 3 Part 2
    I really wish that Python had core methods to unions, differences, and
    intersection.

--- day-03/app.py
+++ day-03/app.py
@@ -40,14 +40,27 @@ def compare_strings(left, right):
 
     """
     d = difflib.Differ()
-    diff_result = d.compare(parts[0], parts[1])
+    diff_result = d.compare(left, right)
     match = ''
+    letter = ''
     for item in diff_result:
         if item[0:1] == ' ':
             letter = item.strip()
             if letter not in match:
                 match += letter
     return match
+
+def string_union(leftstr, rightstr):
+    """TODO: Docstring for string_union.
+    :returns: TODO
+
+    """
+    result = ''
+    for left in leftstr:
+        for right in rightstr:
+            if left == right and left not in result:
+                result += left
+    return result
 
 def calc_priority_list():
     """TODO: Docstring for calc_priority_list.
@@ -68,7 +81,7 @@ def calc_priority_list():
 # Main
 ############################################################
 
-data_file = "day-03/test-input.txt"
+data_file = "day-03/input.txt"
 
 ############
 #  Part I  #
@@ -93,6 +106,42 @@ f.close()
 print('Part I total_score=', total_score)
 
 
+#############
+#  Part II  #
+#############
 
+f = open(data_file,"r")
+
+priorities = calc_priority_list()
+
+total_score = 0
+line_count = 0
+contents = []
+for rucksack in f.readlines():
+    line_count += 1
+    content = rucksack.strip()
+    contents.append(content)
+
+    if line_count == 3:
+        result_1 = string_union(contents[0], contents[1])
+        result_2 = string_union(contents[0], contents[2])
+        result = string_union(result_1, result_2)
+
+        rucksack_score = priorities[result]
+
+        total_score += rucksack_score
+
+        # print('contents=', contents)
+        # print('result_1=', result_1, 'result_2=', result_2, 'result=', result)
+        # print('rucksack_score', rucksack_score)
+        result_1 = ''
+        result_2 = ''
+        result = ''
+        contents = []
+        line_count = 0
+        rucksack_score = 0
+f.close()
+
+print('Part II total_score=', total_score)
 
 
